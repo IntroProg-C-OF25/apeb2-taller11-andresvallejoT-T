4726704109ae48b7e573a16996340715cb93e394
andresvallejoT-T <andresvallejo102006@gmail.com>
2025-01-09 12:35:16 -0500

    correguido xd

--- taller11/src/problema2.java
+++ taller11/src/problema2.java
@@ -11,42 +11,47 @@ El área del rectángulo es igual a base x altura
  * @author Andrés
  */
 import java.util.Scanner;
+
 public class problema2 {
+
     public static void main(String[] args) {
         Scanner tcl = new Scanner(System.in);
         String re;
-        double b, h, ra;
+        double b, h;
         System.out.println("Dame la inicial de la area que deseas (t,r,c)");
-        re=tcl.next();
+        re = tcl.next();
         switch (re) {
             case "t":
                 System.out.println("Dame la base y la altura: ");
-                b=tcl.nextDouble();
-                h=tcl.nextDouble();
-                System.out.println("La area es: "+areatriangulo(b, h));
+                b = tcl.nextDouble();
+                h = tcl.nextDouble();
+                System.out.println("La area es: " + areatriangulo(b, h));
                 break;
             case "r":
                 System.out.println("Dame la base y altura");
-                b=tcl.nextDouble();
-                h=tcl.nextDouble();
-                System.out.println("La area es: "+arearec(b, h));
+                b = tcl.nextDouble();
+                h = tcl.nextDouble();
+                System.out.println("La area es: " + arearec(b, h));
                 break;
             case "c":
-                System.out.println("Dame el radio");
-                ra=tcl.nextDouble();
-                System.out.println("La area es: "+areacir(ra));
+                System.out.println("Dame un lado");
+                b = tcl.nextDouble();
+                System.out.println("La area es: " + areacuad(b));
                 break;
             default:
                 System.out.println("no tenemos esa figura");
         }
     }
-    public static double areatriangulo(double b, double h){
-    return (b*h)/2;
+
+    public static double areatriangulo(double b, double h) {
+        return (b * h) / 2;
     }
-    public static double arearec(double b, double h){
-    return (b*h);
+
+    public static double arearec(double b, double h) {
+        return (b * h);
     }
-    public static double areacir(double r){
-    return 3.14 *r*r;
+
+    public static double areacuad(double r) {
+        return r * r;
     }
 }
